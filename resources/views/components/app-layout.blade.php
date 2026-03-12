113bdf29e50c51400e0c96f1296815e0af081471
syahid <syahidshun7@gmail.com>
2026-03-12 13:11:48 +0800

    add vendor for production

---
+++ resources/views/components/app-layout.blade.php
@@ -0,0 +1,15 @@
+@props(['header' => null])
+
+<div class="min-h-screen bg-gray-100">
+    @isset($header)
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endisset
+
+    <main>
+        {{ $slot }}
+    </main>
+</div>
